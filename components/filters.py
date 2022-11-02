8816dc3b896b58833688321930c096465e89337a
Tom Camp <tom.camp@civicactions.com>
2022-11-02 09:50:10 -0400

    Updating Component List view and Components.

--- components/filters.py
+++ components/filters.py
@@ -10,7 +10,9 @@ class ComponentFilter(filters.FilterSet):
 
     search = filters.CharFilter(method="keyword_search", label="Search")
     type = filters.CharFilter(lookup_expr="iexact")
-    catalog_version = filters.CharFilter(field_name="supported_catalog_versions", method="filter_version")
+    catalog_version = filters.CharFilter(
+        field_name="supported_catalog_versions", method="filter_version"
+    )
 
     class Meta:
         model = Component
@@ -22,23 +24,23 @@ class ComponentFilter(filters.FilterSet):
         )
 
     def filter_version(self, queryset, name, value):
-        return queryset.filter(**{name: [value]})
+        return queryset.filter(supported_catalog_versions__overlap=[value])
 
 
 class ComponentPermissionsFilter(BaseFilterBackend):
     """A filter backend that limits results to those where the requesting user
     has read object level permissions.
     """
-    perm_format = '%(app_label)s.view_%(model_name)s'
+
+    perm_format = "%(app_label)s.view_%(model_name)s"
 
     def filter_queryset(self, request, queryset, view):
         user = request.user
         permission = self.perm_format % {
-            'app_label': queryset.model._meta.app_label,
-            'model_name': queryset.model._meta.model_name,
+            "app_label": queryset.model._meta.app_label,
+            "model_name": queryset.model._meta.model_name,
         }
 
-        return (
-            get_objects_for_user(user, permission, queryset, accept_global_perms=False)
-            .union(queryset.filter(status=Component.Status.PUBLIC))
-        )
+        return get_objects_for_user(
+            user, permission, queryset, accept_global_perms=False
+        ).union(queryset.filter(status=Component.Status.PUBLIC))
